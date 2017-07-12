939ffa6ea9e8dbe9f2d9dbfa7fd394e9cb84e14b
Zachary Morel <morelz133@gmail.com>
2017-07-12 19:20:03 -0400

    Final Project done according to Mock Up

--- main.js
+++ main.js
@@ -13,22 +13,27 @@ people.forEach((currentItem, index) => {
 
   const _names = document.createElement('p');
   _names.textContent = `${currentItem.name.first} ${currentItem.name.last}`;
+  _names.classList.add('boldName');
   _li.appendChild(_names);
 
   const _email = document.createElement('p');
   _email.textContent = `${currentItem.email}`;
+  _email.classList.add('emailFont');
   _li.appendChild(_email);
 
   const _addressOne = document.createElement('p');
   _addressOne.textContent = `${currentItem.location.street}`;
+  _addressOne.classList.add('addressTextPhone')
   _li.appendChild(_addressOne);
 
   const _addressTwo = document.createElement('p');
   _addressTwo.textContent = `${currentItem.location.city} ${currentItem.location.state} ${currentItem.location.postcode}`;
+  _addressTwo.classList.add('addressTextPhone')
   _li.appendChild(_addressTwo);
 
   const _phoneNumber = document.createElement('p');
   _phoneNumber.textContent = `${currentItem.phone}`;
+  _phoneNumber.classList.add('addressTextPhone')
   _li.appendChild(_phoneNumber);
 
   const _ss = document.createElement('p');
